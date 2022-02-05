a9b83b7171c6ad9bfb1bd5505515f8b755f48bb4
Miao Lin <miao.lin@Miao-MacBook13.local>
2022-02-05 23:25:26 +0800

    add train eval part

--- RNN_name_classification/RNN_name_classfy.py
+++ RNN_name_classification/RNN_name_classfy.py
@@ -3,9 +3,12 @@ from io import open
 import glob
 import os
 import unicodedata
+import random
+import torch
+import torch.nn as nn
 
-from letter_utils import all_letters
-
+from letter_utils import n_letters, all_letters, line_to_tensor
+from rnn_network import NameClassifyRNN
 
 def find_files(path):
     return glob.glob(path)
@@ -22,6 +25,39 @@ def unicode_to_ascii(s):
 def read_lines(filename):
     lines = open(filename, encoding='utf-8').read().strip().split('\n')
     return [unicode_to_ascii(line) for line in lines]
+
+
+def category_from_output(output):
+    top_n, top_i = output.topk(1)
+    category_i = top_i[0].item()
+    return all_categories[category_i], category_i
+
+
+def random_choice(l):
+    return l[random.randint(0, len(l)-1)]
+
+
+def random_training_example(all_cate, cate_line):
+    cate = random_choice(all_cate)
+    line = random_choice(cate_line[cate])
+    cate_tensor = torch.tensor([all_cate.index(cate)], dtype=torch.long)
+    line_tensor = line_to_tensor(line)
+    return cate, line, cate_tensor, line_tensor
+
+
+def train(model, criterion, lr, cate_tensor, line_tensor):
+    hidden = rnn.init_hidden()
+    model.zero_grad()
+
+    for i in range(line_tensor.size()[0]):
+        output, hidden = model(line_tensor[i], hidden)
+    loss = criterion(output, cate_tensor)
+    loss.backward()
+
+    # manual update the parameters
+    for p in model.parameters():
+        p.data.add_(p.grad.data, alpha=-lr)
+    return output, loss.item()
 
 
 if __name__ == "__main__":
@@ -41,3 +77,26 @@ if __name__ == "__main__":
     print(n_categories)
     print(category_lines['Italian'][:5])
 
+    for i in range(10):
+        cate, line, cate_tensor, line_tensor = random_training_example(all_categories, category_lines)
+        print('category =', cate, '/ line =', line)
+
+    # training the network
+    n_hidden = 128
+    rnn = NameClassifyRNN(n_letters, n_hidden, 18)
+    criterion = nn.NLLLoss()
+    learning_rate = 0.005
+    n_iters = 1000000
+    print_every = 5000
+    plot_every = 10000
+    cur_loss = 0
+
+    for iter in range(1, n_iters + 1):
+        cate, line, cate_tensor, line_tensor = random_training_example(all_categories, category_lines)
+        output, loss = train(rnn, criterion, learning_rate, cate_tensor, line_tensor)
+        cur_loss += loss
+
+        if iter % print_every == 0:
+            guess, guess_i = category_from_output(output)
+            correct = '✓' if guess == cate else '✗ (%s)' % cate
+            print('%d %d%% %.4f %s / %s %s' % (iter, iter / n_iters * 100, loss, line, guess, correct))
